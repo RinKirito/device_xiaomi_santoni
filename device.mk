88de61f1daf10f8fb130e877b4023fbd281f34ff
ErrorNetwork28 <zidan23.05.2016@gmail.com>
2019-05-03 07:39:31 +0000

    santoni: Make Google Sans the default system font

--- device.mk
+++ device.mk
@@ -216,6 +216,15 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GoogleSans
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    $(LOCAL_PATH)fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    $(LOCAL_PATH)fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    $(LOCAL_PATH)fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    $(LOCAL_PATH)fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    $(LOCAL_PATH)fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8937 \
